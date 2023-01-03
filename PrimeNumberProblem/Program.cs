cd61fb5ace4825cf1135e4825a1c8bc7b4074fc3
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-03 14:28:28 +0530

    PrimeNumber

---
+++ PrimeNumberProblem/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PrimeNumberProblem
+{
+   class PrimeNumber
+    {
+        public static void DisplayPrimeNumber()
+        {
+            bool prime = true;
+
+            Console.WriteLine("INPUT ANY NUMBER");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    prime = false;
+                    Console.WriteLine("{0} NUMBER IS NOT A PRIME NUMBER ", num);
+                    break;
+                }
+            }
+            if (prime)
+            {
+                Console.WriteLine("{0} NUMBER IS A PRIME NUMBER ", num);
+                Console.ReadLine();
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrimeNumber.DisplayPrimeNumber();
+        }
+    }
+}
