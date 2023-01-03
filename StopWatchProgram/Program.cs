f549f0ba2649796c60435ad1456ca16e780c0894
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-03 14:30:50 +0530

    StopWatch

---
+++ StopWatchProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace StopWatchProgram
+{
+    public class StopWatch
+    {
+        public static void DisplayTime()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            //used stopwatch class to get elapsed time
+            stopwatch.Start();
+            Thread.Sleep(5000);
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StopWatch.DisplayTime();
+        }
+    }
+}
